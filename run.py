18648ea8008d89199f34cd0625dfbbc061160301
Yangning <792301982@qq.com>
2021-04-18 14:52:15 +0800

    fix:driver config

--- run.py
+++ run.py
@@ -42,7 +42,7 @@ def ChangeCookies(driver,headers):
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--incognito")  # 配置隐私模式
+#options.add_argument("--incognito")  # 配置隐私模式
 # 减少打印
 options.add_argument('log-level=3')
 driver = webdriver.Chrome(options=options)
