1244f57ac28b0c8cbe9ac2e9bdf8d8f40a292a08
Yangning <792301982@qq.com>
2021-05-03 17:54:51 +0800

    feat:jiexi_data更改为requests获取

--- jiexi_data.py
+++ jiexi_data.py
@@ -3,32 +3,52 @@ from selenium import webdriver
 from taobao import drop_down
 import time
 import json
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+}
 
 
-def jiexi_ziye_data(url,xiaoliang):
-    option = webdriver.ChromeOptions()
-    option.add_experimental_option('excludeSwitches', ['enable-automation'])
-    # 无头模式
-    option.add_argument('--headless')
-    driver = webdriver.Chrome(options=option)
-    driver.get(url)
-    driver.implicitly_wait(5)
-    drop_down(driver)
+def jiexi_ziye_data(url, xiaoliang):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    # option = webdriver.ChromeOptions()
+    # option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    # # 无头模式
+    # option.add_argument('--headless')
+    # driver = webdriver.Chrome(options=option)
+    # driver.get(url)
+    # driver.implicitly_wait(5)
+    # drop_down(driver)
     try:
-        if driver.find_element_by_xpath('//*[@id="shopExtra"]/div[1]/a/strong').text:
-            shop_name = driver.find_element_by_xpath(
-                '//*[@id="shopExtra"]/div[1]/a/strong').text
+        #正则匹配js内容
+        d = json.loads(re.findall(r"TShop.Setup\(\r\n([\s\S]*)\);[\s\S]*}\)\(\);", r.text)[0])
+    except:
+        print("正则匹配失败")
+    try:
+        shop_name = soup.select("#shopExtra > div.slogo > a > strong")[0].text
+        # if driver.find_element_by_xpath('//*[@id="shopExtra"]/div[1]/a/strong').text:
+        #     shop_name = driver.find_element_by_xpath(
+        #         '//*[@id="shopExtra"]/div[1]/a/strong').text
     except:
         shop_name = -1
     try:
-        if driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div/h1').text:
-            shop_introduction = driver.find_element_by_xpath(
-                '/html/body/div/div/div/div/div/div/div/div/h1').text
+        shop_introduction = soup.select(
+            "#J_DetailMeta > div.tm-clear > div.tb-property > div > div.tb-detail-hd > h1")[0].text.strip()
+        # if driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div/div/h1').text:
+        #     shop_introduction = driver.find_element_by_xpath(
+        #         '/html/body/div/div/div/div/div/div/div/div/h1').text
     except:
         shop_introduction = -1
     try:
-        if driver.find_element_by_id("J_StrPriceModBox"):
-            price = driver.find_element_by_id("J_StrPriceModBox").text
+        price = d['detail']['defaultItemPrice']
+        # if driver.find_element_by_id("J_StrPriceModBox"):
+        #     price = driver.find_element_by_id("J_StrPriceModBox").text
     except:
         price = -1
     # 销量使用别的浏览器无法打开，需要在已登录的浏览器中进行打开
@@ -37,37 +57,45 @@ def jiexi_ziye_data(url,xiaoliang):
     # else:
     #     xiaoliang = -1
     try:
-        if driver.find_element_by_id('J_BrandAttr'):
-            pinpaimingcheng = driver.find_element_by_id('J_BrandAttr').text
+        pinpaimingcheng = d['itemDO']['brand']
     except:
         pinpaimingcheng = -1
     try:
-        if driver.find_element_by_xpath('//*[@id="shop-info"]/div[2]/div[1]/div[2]/span'):
-            star = driver.find_element_by_xpath(
-                '//*[@id="shop-info"]/div[2]/div[1]/div[2]/span').text
+        # if driver.find_element_by_xpath('//*[@id="shop-info"]/div[2]/div[1]/div[2]/span'):
+        #     star = driver.find_element_by_xpath(
+        #         '//*[@id="shop-info"]/div[2]/div[1]/div[2]/span').text
+        star=soup.select("#shop-info > div.main-info > div:nth-child(1) > div.shopdsr-score.shopdsr-score-up-ctrl > span")[0].text
     except:
         star = -1
     # url = driver.current_url
     try:
-        parent_elem = driver.find_element_by_xpath('//*[@id="J_AttrUL"]')
-        child_elements = parent_elem.find_elements_by_xpath('.//*')
-        attrDict=dict()
-        for conent in child_elements:
-            cList=conent.text.split("：")
-            attrDict[cList[0]]=cList[1]
-            print(conent.text)
+        # parent_elem = driver.find_element_by_xpath('//*[@id="J_AttrUL"]')
+        # child_elements = parent_elem.find_elements_by_xpath('.//*')
+        attr=soup.select("#J_AttrUL")[0].text.split("\n")
+        attrDict = dict()
+        for content in attr:
+            try:
+                content=content.replace("：",":",1) 
+                cList = content.split(":")
+                attrDict[cList[0].strip()] = cList[1].strip()
+                print(content)
+            except:
+                continue
     except:
-        attrDict=dict()
+        attrDict = dict()
+
+    imgs=d['propertyPics']['default']
+
     d = dict()
     d['shop_name'] = shop_name
     d['pinpaimingcheng'] = pinpaimingcheng
     d['shop_introduction'] = shop_introduction
     d['price'] = price
-    d['xiaoliang']=xiaoliang
+    d['xiaoliang'] = xiaoliang
     d['star'] = star
     d['url'] = url
     d['pic'] = list()
-    d['attr']=attrDict
+    d['attr'] = attrDict
     print('店铺名称：', shop_name)
     print('品牌名称：', pinpaimingcheng)
     print('商品介绍：', shop_introduction)
@@ -75,15 +103,16 @@ def jiexi_ziye_data(url,xiaoliang):
     print('月销量：', xiaoliang)
     print('评分：', star)
     print('当前的url是：', url)
-    for link in driver.find_elements_by_xpath("//*[@alt='商品预览图']"):
-        smpicUrl = link.get_attribute('src')
-        bigPicUrl = smpicUrl.replace('60x60q90', '430x430q90')
-        print(bigPicUrl)
-        d['pic'].append(bigPicUrl)
+    
+    for link in imgs:
+        print(link)
+        d['pic'].append(link)
     # with open("data/%s.json" % url.split('=')[-1], "w+") as f:
     #     json.dump(d, f)
-    driver.quit()
+  #  driver.quit()
     return d
 
+
 if __name__ == "__main__":
-    jiexi_ziye_data("https://detail.tmall.com/item.htm?id=639393424095",1)
+    r=jiexi_ziye_data("https://detail.tmall.com/item.htm?id=639067225502", 1)
+    input()
